87bdbe5a9f36b0fb84e382d41841271dc381cb19
ProducED-ed <producedsites@gmail.com>
2025-11-14 19:46:09 +0300

    Update MAIN_BOARD_V5_COM5.ino

--- MAIN_BOARD_V5_COM5/MAIN_BOARD_V5_COM5.ino
+++ MAIN_BOARD_V5_COM5/MAIN_BOARD_V5_COM5.ino
@@ -6009,8 +6009,7 @@ bool digitalReadExpander(int pin, int boardNumber) {
   digitalWrite(pinA, pin & 0b001);
   digitalWrite(pinB, (pin >> 1) & 0b001);
   digitalWrite(pinC, (pin >> 2) & 0b001);
-  // delay(1);
-  delayMicroseconds(10);
+  delay(1);
   return digitalRead(boardNumber);
 }
 
